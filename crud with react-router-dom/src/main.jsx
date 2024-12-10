1dba028316d41e2cc8c04bd609639d95ebd28bdc
ramin98 <59912261+ramin98@users.noreply.github.com>
2024-12-10 11:22:14 +0400

    Add files via upload

--- crud with react-router-dom/src/main.jsx
+++ crud with react-router-dom/src/main.jsx
@@ -1,49 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Bag from "./components/Pages/Bag.jsx";
-import About from "./components/Pages/About.jsx"
-import ServicePage from "./components/Pages/ServicePage.jsx";
-import ServicesPage from "./components/Pages/ServicesPage.jsx";
-import ServiceName from "./components/Pages/ServiceName.jsx";
-import ServiceDescription from "./components/Pages/ServiceDescription.jsx";
-
-const router = createBrowserRouter([
-  {
-    element:<App/>,
-    path:'/',
-    children:[
-      {
-        element:<Bag/>,
-        path:'bag',
-      }, 
-      {
-        element:<About/>,
-        path:'about',
-      },
-      {
-        element:<ServicesPage/>,
-        index:true,
-      },
-      {
-        element:<ServicePage/>,
-        path:'service-page/:serviceId',
-        children:[
-          {
-            element:<ServiceName/>,
-            path:'service-name',
-          }, 
-          {
-            element:<ServiceDescription/>,
-            path:'service-description',
-          },
-        ]
-      }
-    ]
-  }
-])
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router}/>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
