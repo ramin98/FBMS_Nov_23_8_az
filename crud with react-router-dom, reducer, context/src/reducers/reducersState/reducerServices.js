7c25f350b84cc0c1e526c4d64966c53af7640f03
ramin98 <59912261+ramin98@users.noreply.github.com>
2024-12-12 11:52:04 +0400

    Update reducerServices.js

--- crud with react-router-dom, reducer, context/src/reducers/reducersState/reducerServices.js
+++ crud with react-router-dom, reducer, context/src/reducers/reducersState/reducerServices.js
@@ -31,7 +31,59 @@ export function reducerServices(state, action) {
   
     return state;
   }
-  
+
+// way with object
+
+// import { services } from "../../assets/data";
+// import { DELETE_FROM_SERVICES, EDIT_SERVICES } from './actionType';
+
+// export let initialObjectServices = {
+//   services: services,
+// };
+
+// let actionHandlers = {
+//     [DELETE_FROM_SERVICES]: function (state, action) {
+//         const newArr = [...state.services];
+//         const elementIndex = newArr.findIndex((item) => item.id === action.payload);
+
+//         if (elementIndex === -1) {
+//             console.error(`Service with id ${action.payload} not found.`);
+//             return state;
+//         }
+
+//         newArr.splice(elementIndex, 1);
+//         return { ...state, services: newArr };
+//     },
+//     [EDIT_SERVICES]: function (state, action) {
+//         const newArr = [...state.services];
+//         const elementIndex = newArr.findIndex((item) => item.id === action.payload.id);
+
+//         if (elementIndex === -1) {
+//             console.error(`Service with id ${action.payload.id} not found.`);
+//             return state;
+//         }
+
+//         newArr[elementIndex] = {
+//             ...newArr[elementIndex],
+//             ...action.payload.formData,
+//         };
+
+//         return { ...state, services: newArr };
+//     },
+// };
+
+// export function reducerServices(state, action) {
+//     const handler = actionHandlers[action.type];
+//     if (!handler) {
+//         console.error(`Unhandled action type: ${action.type}`);
+//         return state;
+//     }
+
+//     return handler(state, action);
+// }
+
+
+// way with IF
 
 // export function reducerServices(state, action) {
 //   if (action.type === "DELETE FROM SERVICES") {
