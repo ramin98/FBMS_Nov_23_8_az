4a1b26664a29429e790cbce4f231796c2cbd04b0
ramin98 <59912261+ramin98@users.noreply.github.com>
2024-12-17 10:24:21 +0400

    Delete shop/src/reducers/reducerProductsFetchs.js

--- shop/src/reducers/reducerProductsFetchs.js
+++
@@ -1,5 +0,0 @@
-export async function getProducts(dispatch){
-    let res = await fetch('http://localhost:5000/goods')
-    let data = await res.json()
-    dispatch({type:'GET PRODUCTS', payload: data})
-}
