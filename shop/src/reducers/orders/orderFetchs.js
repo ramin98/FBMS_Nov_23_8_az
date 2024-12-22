7b29a5c8fa18c261643b367d1d0558942c360e73
ramin98 <59912261+ramin98@users.noreply.github.com>
2024-12-22 11:38:40 +0400

    Delete shop/src/reducers directory

--- shop/src/reducers/orders/orderFetchs.js
+++
@@ -1,5 +0,0 @@
-export async function getOrders(dispatch){
-    let res = await fetch('http://localhost:5000/orders')
-    let data = await res.json()
-    dispatch({type:'GET ORDERS', payload: data})
-}
